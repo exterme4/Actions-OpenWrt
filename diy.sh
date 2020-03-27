eab1d1622ac8afd0a7cce38ebc8b350014656ba0
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-03-27 10:24:33 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: P3TERX
-# Blog: https://p3terx.com
-#=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/0x1E00000/0x7c40000/g' target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
