619fd771bfca79977dbe6dced4021193918183b6
Yuki Kikuchi <bclswl0827@foxmail.com>
2020-06-06 13:54:58 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed -i 's/#src-git helloworld https://github.com/fw876/helloworld/src-git helloworld https://github.com/fw876/helloworld/g' feeds.conf.default
+sed -i 's/#src-git/src-git/g' feeds.conf.default
 sed -i 's/0x1E00000/0x7c40000/g' target/linux/ramips/dts/mt7621_hiwifi_hc5962.dts
